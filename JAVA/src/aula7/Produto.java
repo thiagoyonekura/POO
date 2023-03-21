e050592cd58a35cde02b82d3477dee4ce76b54cb
Thiago Yonekura <thiagoyonekura@gmail.com>
2023-03-21 19:49:59 -0300

    código de aula 7

--- JAVA/src/aula7/Produto.java
+++ JAVA/src/aula7/Produto.java
@@ -1,5 +1,18 @@
 package aula7;
-
+//Nome da classe igual ao do arquivo
 public class Produto {
-
+	// atributos da classe
+	String nome;
+	double preco;
+	int quantidade;
+	
+	//métodos da classe
+	public double calculaDesconto() {
+		double desconto = 0.0;
+		if(this.quantidade > 10 && this.quantidade <= 20) {
+			desconto = (this.quantidade * this.preco) * 0.9;
+		}
+		return desconto;
+	}
+	
 }
--- JAVA/src/aula7/Sistema.java
+++ JAVA/src/aula7/Sistema.java
@@ -1,5 +1,19 @@
 package aula7;
 
 public class Sistema {
-
+	public static void main(String args[]) {
+		System.out.println("Olá!");
+		Produto objeto1 = new Produto();
+		objeto1.nome = "Cerveja";
+		objeto1.preco = 3.99;
+		objeto1.quantidade = 12;
+		
+		Produto objeto2 = new Produto();
+		objeto2.nome = "Espetinho";
+		objeto2.preco = 6.00;
+		objeto2.quantidade = 2;
+		System.out.println("O produto "+ objeto2.nome
+				 + " recebeu desconto de R$" + objeto2.calculaDesconto());
+		
+	}
 }
